e13a7fe8c782db8df9abce71c928b3e2762a0fbe
ammarja20 <ammar.ja.20@gmail.com>
2025-09-24 19:40:16 +0300

    Assignment 4 done i guess

---
+++ Api/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Core.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters
+{
+    public class ApiExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            int statusCode = 500;
+            string message = "An unexpected error occurred.";
+
+            if (context.Exception is NotFoundException nf)
+            {
+                statusCode = 404;
+                message = nf.Message;
+            }
+            else if (context.Exception is BusinessException be)
+            {
+                statusCode = 400;
+                message = be.Message;
+            }
+
+            context.Result = new ObjectResult(new { error = message })
+            {
+                StatusCode = statusCode
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- Core/Forms/CreateStudentForm.cs
+++ Core/Forms/CreateStudentForm.cs
@@ -1,8 +1,15 @@
-﻿namespace Core.Forms
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Forms
 {
     public class CreateStudentForm
     {
+        [Required]
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters long.")]
         public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Email must be a valid email address.")]
         public string Email { get; set; } = null!;
     }
 }
